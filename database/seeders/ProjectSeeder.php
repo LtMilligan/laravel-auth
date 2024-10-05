1b662ace32258912322580323c22923481aca37d
LtMilligan <gioele.miscia@gmail.com>
2024-10-05 02:23:08 +0200

    [UPD] Create seeder with slug

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+use App\Models\Project;
+
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<10;  $i++){
+            $project = new Project();
+            $project->name = $faker->sentence(3);
+            $project->summary = $faker->text(5);
+
+            $project->slug = Str::slug($project->name, '-');
+
+            $project->save();
+    }
+}
